d07525e2c074be182e84a0082ae743ae262a92f6
Calvin Lee <clee2691@gmail.com>
2022-03-29 20:36:09 -0400

    refactored api url

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
-const TUITS_API = 'http://localhost:4000/api/tuits';
+//const TUITS_API = 'http://localhost:4000/api/tuits';
+//const TUITS_API = 'https://full-stack-developer-server-cl.herokuapp.com/api/tuits';
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit);
     return response.data;
 }
 export const findAllTuits = async () => {
+    console.log(process.env)
+    console.log(TUITS_API);
     const response = await axios.get(TUITS_API);
     const tuits = response.data;
     return tuits;
