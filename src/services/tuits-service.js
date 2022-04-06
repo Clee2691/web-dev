bbc2914f8e828c4412cddf3cefea502e7bf2a84d
Calvin Lee <clee2691@gmail.com>
2022-04-06 14:44:51 -0400

    Updated A9 API link

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //const TUITS_API = 'http://localhost:4000/api/tuits';
 //const TUITS_API = 'https://full-stack-developer-server-cl.herokuapp.com/api/tuits';
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = process.env.REACT_APP_API_BASE_A9;
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
